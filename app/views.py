b3fbbe27868c9a0bcfe32c2a13070bbc8872a206
mitsui93 <mitsui93@foxmail.com>
2017-08-24 10:37:19 +0800

    form表单完善

--- app/views.py
+++ app/views.py
@@ -27,5 +27,6 @@ def login():
         return redirect('/index')
 
     return render_template('login.html',
-                           title = 'Sign In',
-                           form = form)
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
